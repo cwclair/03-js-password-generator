5f87ea962f72fb51ce379e06670b8c4033b27627
Christopher Clair <124112751+cwclair@users.noreply.github.com>
2023-04-15 09:27:07 -0500

    Added various conditions to password generation

--- script.js
+++ script.js
@@ -2,6 +2,48 @@
 const alphaLowerOnly = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 console.log(alphaLowerOnly);
 
+const alphaUpperOnly = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+console.log(alphaUpperOnly);
+
+const numOptions = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+console.log(numOptions);
+
+const specialCharsList = ["!", "@", "$", "#", "%", "&", "*", "(", ")", "/"];
+console.log(specialCharsList);
+
+const alphaMixedCase = alphaLowerOnly.concat(alphaUpperOnly);
+console.log(alphaMixedCase);
+
+const alphaLowerNum = alphaLowerOnly.concat(numOptions);
+console.log(alphaLowerNum);
+
+const alphaUpperNum = alphaUpperOnly.concat(numOptions);
+console.log(alphaUpperNum);
+
+const alphaAllAndNums = alphaLowerOnly.concat(alphaUpperOnly, numOptions);
+console.log(alphaAllAndNums);
+
+const alphaLowerSpecial = alphaLowerOnly.concat(specialCharsList);
+console.log(alphaLowerSpecial);
+
+const alphaUpperSpecial = alphaUpperOnly.concat(specialCharsList);
+console.log(alphaUpperSpecial);
+
+const numSpecial = numOptions.concat(specialCharsList);
+console.log(numSpecial);
+
+const alphaAllAndSpecial = alphaLowerOnly.concat(alphaUpperOnly, specialCharsList);
+console.log(alphaAllAndSpecial);
+
+const alphaLowerNumSpec = alphaLowerOnly.concat(numOptions, specialCharsList);
+console.log(alphaLowerNumSpec);
+
+const alphaUpperNumSpec = alphaUpperOnly.concat(numOptions, specialCharsList);
+console.log(alphaUpperNumSpec);
+
+const allChars = alphaUpperOnly.concat(alphaLowerOnly, numOptions, specialCharsList);
+console.log(allChars);
+
 
 var generateBtn = document.querySelector("#generate");
 
@@ -42,16 +84,79 @@ console.log(passRequireSpec);
 
 
 //Define the function generatePassword?
+// function generatePassword () {
+//   var theString = "";
+//   var count = 0;
+//   while (count < passLength) {
+//     theString += alphaLowerOnly[Math.floor(Math.random() * alphaLowerOnly.length)];
+//     count ++;
+//   }
+//   return theString;
+// }
+// console.log(generatePassword());
+
+
+//Define the function generatePassword?
 function generatePassword () {
-  var result = "";
+  var theString = "";
   var count = 0;
+  if (passRequireLower === "Y" && passRequireCap === "N" && passRequireNum === "N" && passRequireSpec == "N") {
   while (count < passLength) {
-    result += alphaLowerOnly[Math.floor(Math.random() * alphaLowerOnly.length)];
+    theString += alphaLowerOnly[Math.floor(Math.random() * alphaLowerOnly.length)];
     count ++;
   }
-  return result;
+} else if (passRequireLower === "Y" && passRequireCap === "Y" && passRequireNum === "N" && passRequireSpec == "N") {
+  while (count < passLength) {
+    theString += alphaMixedCase[Math.floor(Math.random() * alphaMixedCase.length)];
+    count ++;
+  }
+} else if (passRequireLower === "Y" && passRequireCap === "N" && passRequireNum === "Y" && passRequireSpec == "N") {
+  while (count < passLength) {
+    theString += alphaLowerNum[Math.floor(Math.random() * alphaLowerNum.length)];
+    count ++;
+  }
+} else if (passRequireLower === "Y" && passRequireCap === "N" && passRequireNum === "N" && passRequireSpec == "Y") {
+  while (count < passLength) {
+    theString += alphaLowerSpecial[Math.floor(Math.random() * alphaLowerSpecial.length)];
+    count ++;
+  }
+} else if (passRequireLower === "Y" && passRequireCap === "Y" && passRequireNum === "Y" && passRequireSpec == "N") {
+  while (count < passLength) {
+    theString += alphaAllAndNums[Math.floor(Math.random() * alphaAllAndNums.length)];
+    count ++;
+  }
+} else if (passRequireLower === "Y" && passRequireCap === "Y" && passRequireNum === "N" && passRequireSpec == "Y") {
+  while (count < passLength) {
+    theString += alphaAllAndSpecial[Math.floor(Math.random() * alphaAllAndSpecial.length)];
+    count ++;
+  }
+} else if (passRequireLower === "Y" && passRequireCap === "N" && passRequireNum === "Y" && passRequireSpec == "Y") {
+  while (count < passLength) {
+    theString += alphaLowerNumSpec[Math.floor(Math.random() * alphaLowerNumSpec.length)];
+    count ++;
+  }
+} else if (passRequireLower === "Y" && passRequireCap === "Y" && passRequireNum === "Y" && passRequireSpec == "Y") {
+  while (count < passLength) {
+    theString += allChars[Math.floor(Math.random() * allChars.length)];
+    count ++;
+  }
+}
+return theString;
 }
 console.log(generatePassword());
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // Write password to the #password input
 function writePassword() {
