6d005c48dcd3bb2943a2522ae96a1b997b6bc0bd
Christopher Clair <124112751+cwclair@users.noreply.github.com>
2023-04-14 22:40:38 -0500

    Added code enforcing password length constraints

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
 // Assignment Code
+
 var generateBtn = document.querySelector("#generate");
 
-var passLength = window.prompt("Enter the length of your password (at least 8 characters, no more than 128 characters).", "8");
+//These prompts ask for password length and the inclusion of character types (lowercase, uppercase, number, special)
+
+
+var passLength = Number(window.prompt("Enter the length of your password (at least 8 characters, no more than 128 characters).", 8));
+
+
+function passLengthCheck() {
+  if (passLength < 8) {
+  window.alert("That's not long enough! Let's go with the minimum of 8 characters.");
+  passLength = 8;
+  console.log(passLength);
+  } else if (passLength > 128) {
+  window.alert("That's not long enough! Let's go with the maximum of 128 characters.");
+  passLength = 128;
+  console.log(passLength);
+} else {
 console.log(passLength);
+}
+}
+passLengthCheck()
 
 var passRequireLower = window.prompt("Do you want your password to include at least one capital letter? (Y/N)", "Y");
 console.log(passRequireLower);
@@ -13,7 +32,7 @@ console.log(passRequireCap);
 var passRequireNum = window.prompt("Do you want your password to include at least one number? (Y/N)", "Y");
 console.log(passRequireNum);
 
-var passRequireSpec = window.prompt("Do you want your password to include at least one special character, like a question mark, exclamation point, etc.? (Y/N)", "Y");
+var passRequireSpec = window.prompt("Do you want your password to include at least one special character, such as a question mark, exclamation point, etc.? (Y/N)", "Y");
 console.log(passRequireSpec);
 
 
