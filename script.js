4744a8e23534b9109b0050c54df9d98f8ddf928f
Christopher Clair <124112751+cwclair@users.noreply.github.com>
2023-04-14 21:43:04 -0500

    Created prompts for password length and char types

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var passLength = window.prompt("Enter the length of your password (at least 8 characters, no more than 128 characters).", "8");
+console.log(passLength);
+
+var passRequireLower = window.prompt("Do you want your password to include at least one capital letter? (Y/N)", "Y");
+console.log(passRequireLower);
+
+var passRequireCap = window.prompt("Do you want your password to include at least one capital letter? (Y/N)", "Y");
+console.log(passRequireCap);
+
+var passRequireNum = window.prompt("Do you want your password to include at least one number? (Y/N)", "Y");
+console.log(passRequireNum);
+
+var passRequireSpec = window.prompt("Do you want your password to include at least one special character, like a question mark, exclamation point, etc.? (Y/N)", "Y");
+console.log(passRequireSpec);
+
+
+
+
+//Define the function generatePassword?
+function generatePassword () {
+  
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
